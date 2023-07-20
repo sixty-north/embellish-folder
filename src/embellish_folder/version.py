422f725c5a09fa6530a36680cad8579d13751fc6
Robert Smallshire <rob@sixty-north.com>
2023-07-20 13:49:58 +0200

    Bump version: 0.10.0 → 0.11.0

--- src/embellish_folder/version.py
+++ src/embellish_folder/version.py
@@ -2,6 +2,6 @@ from collections import namedtuple
 
 Version = namedtuple("Version", ["major", "minor", "patch"])
 
-__version__ = "0.10.0"
+__version__ = "0.11.0"
 __version_info__ = Version(*(__version__.split(".")))
 
